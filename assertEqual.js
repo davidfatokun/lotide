30d811af5f260cc70eec561116cbfc78617a4e0a
David <fatokundavid2@gmail.com>
2023-06-01 18:33:53 -0600

    Added template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  let result = '🛑🛑🛑 Assertion Failed: ' + actual + ' !== ' + expected;
+  let result = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
   if (actual === expected) {
-    result = '✅✅✅ Assertion Passed: ' + actual + ' === ' + expected;
+    result = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
   }
   console.log(result);
 };
