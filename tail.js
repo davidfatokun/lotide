ad851ee2009c3ad49ce0e1a9311085319d0b2b70
David <fatokundavid2@gmail.com>
2023-06-06 02:06:33 -0600

    refactored middle and tail tests

--- tail.js
+++ tail.js
@@ -3,7 +3,6 @@ const tail = function(queue) {
   if(queue.length < 2) return result;
   for (let i = 1; i < queue.length; i++) {
     result.push(queue[i]);
-    assertEqual(result[i-1], queue[i])
   }
   return result;
 };
